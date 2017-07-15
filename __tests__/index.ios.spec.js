ce8c60e04eec84a844744a5d890c900a22689b4f
Nicolas Hodicq <nartawak@gmail.com>
2017-07-15 17:25:42 +0200

    Delete snapshot tests

--- __tests__/index.ios.spec.js
+++ __tests__/index.ios.spec.js
@@ -1,16 +1,12 @@
 import 'react-native';
 import React from 'react';
-import Index from '../index.ios.js';
-
-// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import Index from '../index.ios';
 
 jest.mock('ScrollView', () => jest.genMockFromModule('ScrollView'));
 
 it('renders correctly', () => {
-  const tree = renderer.create(
-    <Index />
-  ).toJSON();
-  
-  expect(tree).toMatchSnapshot();
+  renderer.create(
+    <Index />,
+  );
 });
